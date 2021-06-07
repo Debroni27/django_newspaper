de6d9f778428b8965863230b49c0e8b3cdfad3b3
Ilya <73151889+Debroni27@users.noreply.github.com>
2021-06-07 13:49:17 +0700

    Add files via upload

--- NewsPaper/news_paper/urls.py
+++ NewsPaper/news_paper/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import NewsList, NewsDetail, PostsFiltered, PostAdd, PostEdit,\
     PostDelete, author_me, subscribe_me, NewsCategoryList
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
-    path('', NewsList.as_view()),
+    path('',cache_page(60)(NewsList.as_view())),
     path('<int:pk>', NewsDetail.as_view(), name='detail_news'),
-    path('category/<int:pk>/', NewsCategoryList.as_view(),
+    path('category/<int:pk>/', cache_page(60*5)(NewsCategoryList.as_view()),
          name='category'),
     path('search/', PostsFiltered.as_view()),
     path('add/', PostAdd.as_view(), name='news_add'),
