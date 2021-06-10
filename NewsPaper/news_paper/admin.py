c12eecc277ee5288df0825ba7cb5ba008ef4a017
Ilya <73151889+Debroni27@users.noreply.github.com>
2021-06-10 19:09:48 +0700

    Add files via upload

--- NewsPaper/news_paper/admin.py
+++ NewsPaper/news_paper/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Post, Author, Category, PostCategory, Comment, Subs_sender
 
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'rating')
+    list_filter = ('categories', 'type', 'created_data')
+    search_fields = ('author', 'categories__name')
+
 admin.site.register(Post)
 admin.site.register(Author)
 admin.site.register(Category)
